363f43952c8e4dde36a6ab25661d9608bf4fe155
Adam Morelli <amorelli@gmail.com>
2025-01-04 16:28:14 -0500

    Modify config and make more cooler

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -11,27 +11,30 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <header className="mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
+        <div className="container flex flex-col items-start justify-between mx-auto md:flex-row md:items-center">
+          <a className="text-lg font-bold" href="/">Adam Morelli</a>
+          <nav
+            className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+            id="nav"
+          >
+            <div className="flex flex-row space-x-0">
+              {Object.entries(navItems).map(([path, { name }]) => {
+                return (
+                  <Link
+                    key={path}
+                    href={path}
+                    className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  >
+                    {name}
+                  </Link>
+                )
+              })}
+            </div>
+          </nav>
           </div>
-        </nav>
       </div>
-    </aside>
+    </header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { BlogPosts } from 'app/components/posts'
+import skullWebp from './images/skull.webp'
+import spaghetti from './images/spagh.png'
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Adam Morelli
-      </h1>
+      {/* <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Hi, I'm Adam.
+      </h1> */}
+      {/* <img src='skull.webp' alt="Skull Gif" /> */}
+      {/* <img src='spagh.png' alt="Spaghetti" /> */}
       <p className="mb-4">
-        {`Hi, I'm Adam, and this is my site. I work as a web developer, primarily using React, Laravel, MySQL, GraphQL, and AWS.`}
+        {`I work as a web developer, primarily using React, Laravel, MySQL, GraphQL, and AWS.`}
       </p>
       <div className="my-8">
         <BlogPosts />
