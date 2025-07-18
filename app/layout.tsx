7a1eab7623ca542ea7551da4a85af72bade3ae66
Adam <adam@pop-os.localdomain>
2025-07-18 00:30:58 -0400

    Update bg styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,12 +48,13 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        // 'text-black bg-white dark:text-white dark:bg-black',
+        'text-black',
         GeistSans.variable,
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-4xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-4xl lg:mx-auto">
         <GoogleAnalytics gaId="G-CGTTNSTVQ9" />
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
